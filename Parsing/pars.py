fabeefa05774b1ab6d87ff3ae5c3bfa12267aac4
FireRalin <42908420+FireRalin@users.noreply.github.com>
2020-10-29 14:54:32 +0300

    small fix

--- Parsing/pars.py
+++ Parsing/pars.py
@@ -16,9 +16,9 @@ python = open('python.txt', 'w')
 
 
 count = 0
-syblolsClanguage = 0
-syblolsJavalanguage = 0
-syblolsPythonlanguage = 0
+symbolsClanguage = 0
+symbolsJavalanguage = 0
+symbolsPythonlanguage = 0
 
 try: 
     for line in f:
@@ -27,7 +27,6 @@ try:
         soup = BeautifulSoup(xml, features="xml")
         #print(soup.prettify)
         if soup.pre:
-            o.write(str(soup.pre) + '\n')
             for link in soup.find_all('pre'):
                 if (link.get('class') != None and link.get('class') != 'lang-none prettyprint-override'):
                     print("Language - ", link.get('class'))
@@ -35,21 +34,21 @@ try:
                     #print("\n")
                     o.write(str(soup.pre) + '\n')
                     if(link.get('class') == 'lang-c prettyprint-override' or link.get('class') == 'lang-objective-c prettyprint-override' or link.get('class') == 'lang-cpp prettyprint-override'):
-                        syblolsClanguage += len(str(soup.pre))
+                        symbolsClanguage += len(str(soup.pre))
                         c.write(str(soup.pre) + '\n')
                     elif(link.get('class') == 'lang-java prettyprint-override' or link.get('class') == 'lang-js prettyprint-override'):
-                        syblolsJavalanguage += len(str(soup.pre))
+                        symbolsJavalanguage += len(str(soup.pre))
                         java.write(str(soup.pre) + '\n')
                     elif(link.get('class') == 'lang-py prettyprint-override'):
-                        syblolsPythonlanguage += len(str(soup.pre))
+                        symbolsPythonlanguage += len(str(soup.pre))
                         python.write(str(soup.pre) + '\n')
                     
 except Exception:
     print(' ')
 
-print("Parse ", int(syblolsClanguage/65), " lines on C language")
-print("Parse ", int(syblolsJavalanguage/65), " lines on Java language")
-print("Parse ", int(syblolsPythonlanguage/65), " lines on Python language")
+print("Parse ", int(symbolsClanguage/65), " lines on C language")
+print("Parse ", int(symbolsJavalanguage/65), " lines on Java language")
+print("Parse ", int(symbolsPythonlanguage/65), " lines on Python language")
 print("Posts with <pre> ",count)
 f.close()
 o.close()
