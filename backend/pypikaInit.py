6cf83a13c331d7ca326840cc2c712baf3f90971b
Alexander <alexandr-altukhov2013@yandex.ru>
2020-11-16 00:03:12 +0300

    feat: algorithm

--- backend/pypikaInit.py
+++ backend/pypikaInit.py
@@ -9,5 +9,6 @@ class DBObjects:
 		self.func = {}
 		self.func["metaphone"] = CustomFunction('metaphone', ['text', 'len'])
 		self.func["levenshtein"] = CustomFunction('levenshtein', ['source', 'target'])
+		self.func["levenshtein_less_equal"] = CustomFunction('levenshtein_less_equal', ['source', 'target', 'max_d'])
 
 db = DBObjects()
